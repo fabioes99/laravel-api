2e01f0d49733ee6aecd5a68f88573d195eb0a1a8
fabioes99 <fabioes.99@outlook.com>
2023-09-29 20:25:43 -0300

    filtrando data pela url

--- app/Http/Controllers/Api/v1/CustomerController.php
+++ app/Http/Controllers/Api/v1/CustomerController.php
@@ -7,15 +7,33 @@ use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\CustomerResource;
+use App\Http\Resources\V1\CustomerCollection;
+use App\Services\V1\CustomerQuery;
+use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     *@return \Illuminate\Http\Response
      */
-    public function index()
+
+    public function index(Request $request)
     {
-       return Customer::all();
+        $filter = new CustomerQuery();
+        $queryItems = $filter->transform($request); // [['column', 'operator', 'value']]
+
+        if( count($queryItems) == 0 ){
+
+            return new CustomerCollection(Customer::paginate());
+        }else{
+            return new CustomerCollection(Customer::where($queryItems)->paginate());
+        }
+
+
+
+
     }
 
     /**
