70fe44324b646303b6fedd3896d678fe6ff307bb
fabioes99 <fabioes.99@outlook.com>
2023-10-05 18:52:18 -0300

    adding put and patch

--- app/Http/Requests/V1/StoreCustomerRequest.php
+++ app/Http/Requests/V1/StoreCustomerRequest.php
@@ -34,8 +34,11 @@ class StoreCustomerRequest extends FormRequest
     }
 
     protected function prepareForValidation(){
-        $this->merge([
-            'postal_code' => $this->postalCode
-        ]);
+        if($this->postalCode){
+            $this->merge([
+                'postal_code' => $this->postalCode
+            ]);
+        }
+
     }
 }
